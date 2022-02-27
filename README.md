59d626b10cab12db23f0e5d04ce33f35c376937e
deadpixelsclub <deadpixelsclub@gmail.com>
2022-02-26 23:02:03 -0600

    update

--- README.md
+++ README.md
@@ -33,18 +33,18 @@ This is an example `config.py` file:
 
 ```
 {
-  "trait_type_order": ["background", "eyes", "mouth"], 
   "n": 5, 
+  "image_size": (1410, 1410)
+  "name_prefix": "#", 
+  "description": "Description of project.",
+  "trait_base_path": "./traits/",
+  "trait_type_order": ["background", "eyes", "mouth"], 
+  "base_image_uri": "ipfs://NewUriToReplace/", 
   "conflicts": { 
       "solid_blue": ["expression_eyes", "expression_smirk"],
       "expression_smirk": ["eyewear_glasses_aviator"],
     },
   "skip_similar": "background",
-  "trait_base_path": "./traits/",
-  "base_image_uri": "ipfs://NewUriToReplace/", 
-  "image_size": (1410, 1410)
-  "name_prefix": "#", 
-  "description": "Description of project.",
   "additional_metadata": [{"creator": "Your Name"},
                           {"supply": 5}]
 }
--- config.py
+++ config.py
@@ -1,23 +1,16 @@
 config = {
-  "trait_type_order": ["background", "eyes", "mouth"], # Change to reflect your projects trait types
-  "n": 9, # Number of images to generate
-  "conflicts": { 
-      "trait_name": ["conflicting_trait_name_1", "conflicting_trait_name_2", "conflicting_trait_name_3"], # Images where trait_name appears with any of the conflicting_trait_names will not be generated
-      "trait_name2": ["conflicting_trait_name_1", "conflicting_trait_name_2", "conflicting_trait_name_3"],
-    },
-  "ignore_duplicate_trait": "background",
-  "trait_base_path": "./traits/", # Name of folder containing trait types and their respective trait files
-  "base_image_uri": "ipfs://NewUriToReplace/", 
-  "image_size": (1410, 1410), #(height, width)
-  "name_prefix": "#", # Can be anything
+  "n": 5, 
+  "image_size": (1410, 1410)
+  "name_prefix": "#", 
   "description": "Description of project.",
-  "additional_metadata": [{"creator": ""},  # Values may be added or removed from this list.  The entire field may also be removed.
-                          {"category": ""},  
-                          {"supply": 100},
-                          {"properties": {
-                              "catalog": [],
-                              "extras": [],
-                              }
-                            }
-                          ]
+  "trait_base_path": "./traits/",
+  "trait_type_order": ["background", "eyes", "mouth"], 
+  "base_image_uri": "ipfs://NewUriToReplace/", 
+  "conflicts": { 
+      "solid_blue": ["expression_eyes", "expression_smirk"],
+      "expression_smirk": ["eyewear_glasses_aviator"],
+    },
+  "skip_similar": "background",
+  "additional_metadata": [{"creator": "Your Name"},
+                          {"supply": 5}]
 }
